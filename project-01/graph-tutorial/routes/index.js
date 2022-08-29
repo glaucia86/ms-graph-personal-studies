fbf7aac9429450cdfaf7fbde552e6c35f092c5d5
glaucia86 <glaucia_lemos86@hotmail.com>
2022-08-29 18:10:34 -0300

    changing the home route - index

--- project-01/graph-tutorial/routes/index.js
+++ project-01/graph-tutorial/routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * file: routes/index.js
+ * date: 08/29/2022
+ * description: file responsible for the 'index' route of the api
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  let params = {
+    active: { home: true },
+  };
+
+  res.render('index', params);
 });
 
 module.exports = router;
