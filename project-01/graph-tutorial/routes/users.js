e73c7747798893c859f169e6016653497e75c37c
glaucia86 <glaucia_lemos86@hotmail.com>
2022-09-12 19:35:49 -0300

    updating some auth.js file

--- project-01/graph-tutorial/routes/users.js
+++ project-01/graph-tutorial/routes/users.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var router = express.Router();
+/**
+ * file: routes/users.js
+ * date: 09/12/2022
+ * description: file responsible for the 'users' route of the api
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('respond with a resource');
 });
 
