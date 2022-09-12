dd86771b9ba6acfc1fcf23ba38c9d36d09b8e2f0
glaucia86 <glaucia_lemos86@hotmail.com>
2022-09-12 20:15:57 -0300

    developing 'graph.js' file

---
+++ project-01/graph-tutorial/graph.js
@@ -0,0 +1,58 @@
+/**
+ * file: graph.js
+ * date: 09/12/2022
+ * description: file responsible for graph implementation
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const graph = require('@microsoft/microsoft-graph-client');
+require('isomorphic-fetch');
+
+module.exports = {
+  getUserDetails: async function (msalClient, userId) {
+    const client = getAuthenticatedClient(msalClient, userId);
+
+    const user = await client
+      .api('/me')
+      .select('displayName,mail,mailboxSettings,userPrincipalName')
+      .get();
+
+    return user;
+  },
+};
+
+function getAuthenticatedClient(msalClient, userId) {
+  if (!msalClient || !userId) {
+    throw new Error(
+      `Invalid MSAL state. Client: ${
+        msalClient ? 'present' : 'missing'
+      }, User ID: ${userId ? 'present' : 'missing'}`
+    );
+  }
+
+  // Initialize Graph client
+  const client = graph.Client.init({
+    authProvider: async (done) => {
+      try {
+        const account = await msalClient
+          .getTokenCache()
+          .getAccountByHomeId(userId);
+
+        if (account) {
+          const response = await msalClient.acquireTokenSilent({
+            scopes: process.env.OAUTH_SCOPES.split(','),
+            redirectUri: process.env.OAUTH_REDIRECT_URI,
+            account: account,
+          });
+
+          done(null, response.accessToken);
+        }
+      } catch (error) {
+        console.log(JSON.stringify(error, Object.getOwnPropertyNames(error)));
+        done(error, null);
+      }
+    },
+  });
+
+  return client;
+}
