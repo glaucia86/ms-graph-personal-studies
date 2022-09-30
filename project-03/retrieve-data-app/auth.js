b8c213bec9d4bebcb405c03261837fda11183ba6
glaucia86 <glaucia_lemos86@hotmail.com>
2022-09-30 18:26:54 -0300

    Including some important enviroments

--- project-03/retrieve-data-app/auth.js
+++ project-03/retrieve-data-app/auth.js
@@ -1,50 +1,62 @@
-//MSAL configuration
+/**
+ * file: auth.js
+ * date: 10/04/2022
+ * description: file responsible for authenticating the user.
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
 const msalConfig = {
-    auth: {
-        clientId: '<your client ID here>',
-        // comment out if you use a multi-tenant AAD app
-        authority: 'https://login.microsoftonline.com/<your directory ID here>',
-        redirectUri: 'http://localhost:8080'
-    }
+  auth: {
+    clientId: process.env.AAD_APP_CLIENT_ID,
+    authority: process.env.AAD_APP_DIRECTORY_ID_URL,
+    redirectUri: 'http://localhost:8080',
+  },
 };
 const msalRequest = { scopes: [] };
-function ensureScope (scope) {
-    if (!msalRequest.scopes.some((s) => s.toLowerCase() === scope.toLowerCase())) {
-        msalRequest.scopes.push(scope);
-    }
+function ensureScope(scope) {
+  if (
+    !msalRequest.scopes.some((s) => s.toLowerCase() === scope.toLowerCase())
+  ) {
+    msalRequest.scopes.push(scope);
+  }
 }
 //Initialize MSAL client
 const msalClient = new msal.PublicClientApplication(msalConfig);
 
 // Log the user in
 async function signIn() {
-    const authResult = await msalClient.loginPopup(msalRequest);
-    sessionStorage.setItem('msalAccount', authResult.account.username);
+  const authResult = await msalClient.loginPopup(msalRequest);
+  sessionStorage.setItem('msalAccount', authResult.account.username);
 }
 //Get token from Graph
 async function getToken() {
-    let account = sessionStorage.getItem('msalAccount');
-    if (!account) {
-        throw new Error(
-            'User info cleared from session. Please sign out and sign in again.');
-    }
-    try {
-        // First, attempt to get the token silently
-        const silentRequest = {
-            scopes: msalRequest.scopes,
-            account: msalClient.getAccountByUsername(account)
-        };
+  let account = sessionStorage.getItem('msalAccount');
+  if (!account) {
+    throw new Error(
+      'User info cleared from session. Please sign out and sign in again.'
+    );
+  }
+  try {
+    // First, attempt to get the token silently
+    const silentRequest = {
+      scopes: msalRequest.scopes,
+      account: msalClient.getAccountByUsername(account),
+    };
 
-        const silentResult = await msalClient.acquireTokenSilent(silentRequest);
-        return silentResult.accessToken;
-    } catch (silentError) {
-        // If silent requests fails with InteractionRequiredAuthError,
-        // attempt to get the token interactively
-        if (silentError instanceof msal.InteractionRequiredAuthError) {
-            const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
-            return interactiveResult.accessToken;
-        } else {
-            throw silentError;
-        }
+    const silentResult = await msalClient.acquireTokenSilent(silentRequest);
+    return silentResult.accessToken;
+  } catch (silentError) {
+    // If silent requests fails with InteractionRequiredAuthError,
+    // attempt to get the token interactively
+    if (silentError instanceof msal.InteractionRequiredAuthError) {
+      const interactiveResult = await msalClient.acquireTokenPopup(msalRequest);
+      return interactiveResult.accessToken;
+    } else {
+      throw silentError;
     }
+  }
 }
