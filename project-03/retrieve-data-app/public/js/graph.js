8fca2197b614aef89280db7b2b05d4ae72832fb3
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-04 19:46:18 -0300

    including in the 'graph.js' file a new permission to access email's user by MS Graph.

--- project-03/retrieve-data-app/public/js/graph.js
+++ project-03/retrieve-data-app/public/js/graph.js
@@ -12,10 +12,23 @@ const authProvider = {
     return await getToken();
   },
 };
+
 // Initialize the Graph client
 const graphClient = MicrosoftGraph.Client.initWithMiddleware({ authProvider });
+
 //Get user info from Graph
 async function getUser() {
   ensureScope('user.read');
   return await graphClient.api('/me').select('id,displayName').get();
+}
+
+async function getEmails() {
+  ensureScope('mail.read');
+
+  return await graphClient
+    .api('/me/messages')
+    .select('subject,receivedDateTime')
+    .orderby('receivedDateTime desc')
+    .top(10)
+    .get();
 }
