85345659a100dfb2c0007251914b40ee0fc9658c
glaucia86 <glaucia_lemos86@hotmail.com>
2022-10-18 20:58:35 -0300

    wraping the MsalProvider component

--- project-04/msal-react-tutorial/src/index.js
+++ project-04/msal-react-tutorial/src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { msalConfig } from './auth/authConfig';
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={msalConfig}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
